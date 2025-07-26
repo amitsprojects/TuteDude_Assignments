d20f257dea63fcd45ab89e63cce2bf8de8418438
amitsprojects <108460119+amitsprojects@users.noreply.github.com>
2025-07-26 08:41:04 +0530

    Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -26,7 +26,7 @@ Problem Statement: Write a Python program that:
 
 i = 1
 sum = 0
-for i in range(1,50):
+for i in range(1,51):
     sum = sum + i
 print("The sum of numbers from 1 to 50 is: " + str(sum))
 
